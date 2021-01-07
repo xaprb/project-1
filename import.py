824410008a8c3b0b4994db8daf9a60fde68cfa49
Baron Schwartz <xaprb@users.noreply.github.com>
2021-01-07 15:08:27 -0500

    add support for related records via taxonomy (parent-child-birth)

--- import.py
+++ import.py
@@ -5,25 +5,33 @@ import csv
 import requests
 
 CSV_FILES = {
-    "1860_census_free_blacks": "1kJaSr0Aota8Hp59_aLo8ElKiQFSKP20w",
-    "free_blacks_rockbridge_county": "1LCrMNdAevfAT8n6YvcX2exVaRV8rsxmiZ-E--BggGi0"
+    "1860_census_free_blacks": {
+        "spreadsheet": "1kJaSr0Aota8Hp59_aLo8ElKiQFSKP20w",
+        "pcb": "Interpret-MotherFN-ChildFN-ChildBirthYear",
+    },
+    "free_blacks_rockbridge_county": {
+        "spreadsheet": "1LCrMNdAevfAT8n6YvcX2exVaRV8rsxmiZ-E--BggGi0",
+    },
+    "freedmens_marriage_records": {
+        "spreadsheet": "12Hk_VxV9XR0I3Ccvq1RokF_Nhw2_ilke",
+    },
+    "us_colored_troops_born_in_rc": {
+        "spreadsheet": "1EtGNZgHzr7u8KecPk1vO-ixDS3WQo1NR",
+    }
 }
 
-for key, url in CSV_FILES.items():
+for key, info in CSV_FILES.items():
     directory = "content/" + key + "/"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    # Delete old page files. Leave the _index.md file there.
+    # Delete old page files except _index.md
     for f in os.listdir(directory):
         if not f.startswith("_") and f.endswith(".md"):
             os.remove(directory + f)
 
-    # with open('census.csv', newline='') as csvfile:
-    #   reader = csv.DictReader(csvfile)
-    #   for row in reader:
     i = 0
-    with requests.get("https://docs.google.com/spreadsheets/d/" + url + "/export?format=csv", stream=True) as r:
+    with requests.get("https://docs.google.com/spreadsheets/d/" + info["spreadsheet"] + "/export?format=csv", stream=True) as r:
         if r.status_code >= 400:
             print("Error accessing", key, r.raise_for_status())
             continue
@@ -31,7 +39,15 @@ for key, url in CSV_FILES.items():
         for row in csv.DictReader(lines):
             i = i + 1
             # row["id"] = str(i)
-            row["title"] = row["First Name"] + " " + row["Surname"]
+            frontmatter = {
+                "title": row.get("First Name", "") + " " + row.get("Surname", ""),
+                "kv": row,
+            }
+            if "pcb" in info:
+                tmp = row.pop(info["pcb"], "")
+                if tmp != "":
+                    frontmatter["pcb"] = [tmp]
             new_page = open(os.path.join(directory, str(i) + ".md"), 'w')
-            new_page.write(json.dumps(row, indent=1)+"\n\n")
+            new_page.write(json.dumps(frontmatter, indent=1)+"\n\n")
             new_page.close()
+        print("finished processing", key)
